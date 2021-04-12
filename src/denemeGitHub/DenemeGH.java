10aca0afccd719ba8dcb32d5394c384f9120646d
Alperen <alperencemalsozkeser@gmail.com>
2021-04-13 00:07:09 +0200

    deneme3

--- src/denemeGitHub/DenemeGH.java
+++ src/denemeGitHub/DenemeGH.java
@@ -5,7 +5,7 @@ public class DenemeGH {
 	public static void main(String[] args) {
 		System.out.println("deneme yapýyoruz");
 		System.out.println("tekrar alýþtýrma yapacaðýz");
-
+		System.out.println("alýtýrmya devam");
 	}
 
 }
