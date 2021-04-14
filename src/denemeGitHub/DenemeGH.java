6aeac3f90b91c696e015b98b6a03c7507587dcba
AlperenSozkeser <81852161+AlperenSozkeser@users.noreply.github.com>
2021-04-14 02:56:17 +0200

    Update DenemeGH.java

--- src/denemeGitHub/DenemeGH.java
+++ src/denemeGitHub/DenemeGH.java
@@ -16,6 +16,19 @@ public class DenemeGH {
 		System.out.println("alýþturýmaanlara devam ");
 		System.out.println("ne kadar alýþtýrma o kadar öðrenme");
 	
+		System.out.println("deneme yapýyoruz");
+		System.out.println("tekrar alýþtýrma yapacaðýz");
+		System.out.println("alýtýrmya devam");
+		System.out.println("deneme04");
+		System.out.println("denemeye devam");
+		System.out.println("alýþtýrmaaaa");
+		System.out.println("alýþtýma");
+		System.out.println("alýþtýrma");
+		System.out.println("alýþturýmaanlara devam ");
+		System.out.println("ne kadar alýþtýrma o kadar öðrenme");
+		System.out.println("alýþturýmaanlara devam ");
+		System.out.println("ne kadar alýþtýrma o kadar öðrenme");
+	
 	}
 
 }
